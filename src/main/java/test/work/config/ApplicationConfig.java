6b9f7a123bfb0d24e0208559f0164ac3e5abe08f
svaerdstaal <svaerdstaal@test.test>
2019-06-03 00:22:26 +0200

    Basic Spring Thing

---
+++ src/main/java/test/work/config/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package test.work.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"test.work"})
+public class ApplicationConfig {
+
+}
